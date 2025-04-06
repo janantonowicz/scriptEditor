e129a8c588779b5d01f310e79bbbc661a6f05771
woturios <j4ntonowicz@gmail.com>
2025-04-06 20:22:14 +0200

    Initial Commit

---
+++ scriptInput/scriptInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  scriptInputApp.swift
+//  scriptInput
+//
+//  Created by Jan Antonowicz on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct scriptInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scriptInputTests/scriptInputTests.swift
@@ -0,0 +1,17 @@
+//
+//  scriptInputTests.swift
+//  scriptInputTests
+//
+//  Created by Jan Antonowicz on 06/04/2025.
+//
+
+import Testing
+@testable import scriptInput
+
+struct scriptInputTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
