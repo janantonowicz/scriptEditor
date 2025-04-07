6f29c4337a1689525855d616c22b5eedd050a74f
Jan Antonowicz <jan.antonowicz@gmail.com>
2025-04-07 23:38:39 +0200

    Update UserDefaults.swift
    added more keywords for start

--- scriptInput/Extensions/UserDefaults.swift
+++ scriptInput/Extensions/UserDefaults.swift
@@ -15,7 +15,7 @@ extension UserDefaults {
     var keywords: [String] {
         get {
             return (array(forKey: Keys.keywords) as? [String])
-                ?? ["if", "else", "for", "while", "func", "return", "print", "var", "let"]
+                ?? ["if", "else", "for", "while", "func", "return", "print", "var", "let", "sleep", "import", "match", "case", "do", "try", "catch"]
         }
         set {
             set(newValue, forKey: Keys.keywords)
